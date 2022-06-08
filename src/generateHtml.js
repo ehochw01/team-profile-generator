72e2cc70550e639636aa30292ef311274eaf6d4a
Eric Hochwald <erichochwald@erics-mbp.myfiosgateway.com>
2022-06-08 18:41:48 -0400

    tested writetofile for generated html file

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -5,6 +5,8 @@ const Manager = require("../lib/manager");
 const Team = require("../lib/team");
 const fs = require('fs');
 
+const filename = "../dist/generated.html";
+
 function createTestTeam() {
     const team = new Team();
     team.setManager(new Manager("Jared", 1, "jared@fakemail.com", 1));
@@ -17,7 +19,48 @@ function createTestTeam() {
 
 function createTemplate(team) {
     console.log(team);
+    let template = 
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container-fluid bg-danger p-5">
+        <h1 class="display-6 text-center text-light">My Team</h1>
+    </div>
+    <div class="row m-5 row-cols-1 row-cols-md-3">
+        <div class="col mb-4">
+            <div class="card text-white bg-primary mb-3">
+                <div class="card-header">Manager</div>
+                <div class="card-body">
+                    <h5 class="card-title">Jared</h5>
+                    <p class="card-text">ID: 1</p>
+                    <p class="card-text">Office Number: 1</p>
+                    <p class="card-text">Email: <a href="mailto: jared@fakemail.com" class="text-white">jared@fakemail.com</a></p>
+                </div>
+            </div>
+        </div>`
+
+    template +=
+`    
+    </div>
+</body>
+</html>`
+    return template;
+    
+}
+
+function writeToFile(fileName, fileTemplate) {
+    fs.writeFile(fileName, fileTemplate, (err) =>
+        err ? console.log(err) : console.log('Success!')
+    );
 }
 
 const team = createTestTeam();
-createTemplate(team);
+const fileTemplate = createTemplate(team);
+writeToFile(filename, fileTemplate);
+
